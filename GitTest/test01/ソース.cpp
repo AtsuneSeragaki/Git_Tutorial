9ff8d457a47dd73ca10f4d9ba4a56462e18932ff
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-01-11 09:52:18 +0900

    位置変更

--- GitTest/test01/ソース.cpp
+++ GitTest/test01/ソース.cpp
@@ -2,9 +2,6 @@
 
 int main(void)
 {
-	int num = 5;
-	printf("%d\n", num);
-	printf("Hello Git World!!\n");
 
 	printf("konnichiwa\n");
 	printf("konnichiwa\n");
@@ -12,6 +9,10 @@ int main(void)
 	printf("konnichiwa\n");
 	printf("konnichiwa\n");
 	printf("konnichiwa\n");
+
+	int num = 5;
+	printf("%d\n", num);
+	printf("Hello Git World!!\n");
 
 
 	return 0;
